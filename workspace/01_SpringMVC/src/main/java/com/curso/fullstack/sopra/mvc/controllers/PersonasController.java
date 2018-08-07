3dc559e4e76123a34838da94b2b0ca9e4aee2165
Victor <victorherrerocazurro@gmail.com>
2018-08-07 11:29:12 +0200

    Swagger

--- workspace/01_SpringMVC/src/main/java/com/curso/fullstack/sopra/mvc/controllers/PersonasController.java
+++ workspace/01_SpringMVC/src/main/java/com/curso/fullstack/sopra/mvc/controllers/PersonasController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.curso.fullstack.sopra.mvc.dto.Persona;
@@ -23,7 +25,9 @@ public class PersonasController {
 	
 	@GetMapping("/persona/saludo/{name}")
 	public String saludo(@PathVariable(name="name", required=false) String nombre, 
-			@RequestParam(name="prefix", required=false) String prefijo, Model model) {
+			@RequestParam(name="prefix", required=false) String prefijo, Model model, HttpSession session) {
+		
+		session.setAttribute("nombre", nombre);
 		
 		model.addAttribute("dato", prefijo + " " + nombre + "!!!");
 		
@@ -31,7 +35,9 @@ public class PersonasController {
 	}
 	
 	@PostMapping("/persona/saludo")
-	public ModelAndView saludo2(@RequestBody Persona persona) {
+	public ModelAndView saludo2(
+			@SessionAttribute("nombre") String nombre, 
+			@RequestBody Persona persona) {
 		Map<String, Object> model = new HashMap<>();
 		
 		model.put("dato", "Este es un mensaje para la vista");
